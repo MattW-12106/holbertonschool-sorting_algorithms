cb9dc78a61a82d3b73046ab402f233bec6396659
MattW-12106 <12106@holbertonstudents.com>
2025-12-15 09:46:37 +0000

    adjusted header file to have listint_s struct above the functions that are using it... duh

--- sort.h
+++ sort.h
@@ -3,6 +3,23 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+
+/* Data Structure for DLL */
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 
 /* Functions */
 
@@ -47,22 +64,6 @@ void print_list(const listint_t *list)
     }
     printf("\n");
 }
-
-/* Data Structure for DLL */
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
 
 /* Prototypes */
 
