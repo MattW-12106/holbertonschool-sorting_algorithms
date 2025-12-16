1332bfece61f00a818ce062bfbd08a351eca2812
MattW-12106 <12106@holbertonstudents.com>
2025-12-16 09:11:19 +0000

    quick sort function + complexity cases

---
+++ 3-quick_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers in an array
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * lomuto_partition - Partitions the array using Lomuto scheme
+ */
+
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - Recursive Quick Sort
+ */
+
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	int p;
+
+	if (low < high)
+	{
+		p = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, p - 1, size);
+		quick_sort_recursive(array, p + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Entry point
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
+}
