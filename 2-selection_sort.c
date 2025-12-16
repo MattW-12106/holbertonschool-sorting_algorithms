cb0b82f457afe431395361ca92fe992e00f8745a
MattW-12106 <12106@holbertonstudents.com>
2025-12-16 07:35:19 +0000

    selection sorting algorithm + time complexity cases

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of ints using selection sort
+ * @array - pointer to array
+ * @size - size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != 1)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
